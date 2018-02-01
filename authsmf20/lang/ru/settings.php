76fd53684b22315972f332cc347ede24360136d3
realdigger <digger@mysmf.net>
2018-02-01 07:13:00 +0400

    Update Russian lang.

--- authsmf20/lang/ru/settings.php
+++ authsmf20/lang/ru/settings.php
@@ -2,7 +2,7 @@
 /**
  * Russian translation for the authsmf20 plugin settings.
  *
- * @package SMF DokuWiki
+ * @package SMF DocuWiki
  * @file settings.php
  * @author digger <digger@mysmf.net>
  * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
@@ -10,4 +10,4 @@
  */
 
 $lang['smf_path']   = 'Путь к форуму SMF (можно посмотреть в SMF Админка - Настройки сервера - Абсолютный путь к директории с форумом).';
-$lang['smf_cache']  = 'Cache duration in days (0 will disable the cache).';
+$lang['smf_cache']  = 'Время кэширования в днях (0 отключает кэширование).';
